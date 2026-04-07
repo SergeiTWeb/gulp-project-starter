fbf0f9dffb1aefd5397d022df80002c455cf1427
Sergei Tumanov <sergeitumanov.web@gmail.com>
2026-04-07 16:51:30 +0300

    massive update for plugins and general settings in css

--- task/css.js
+++ task/css.js
@@ -9,10 +9,16 @@ const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
 const concat = require("gulp-concat");
 const cssimport = require("gulp-cssimport");
+const autoprefixer = require('gulp-autoprefixer').default;
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const size = require("gulp-size").default;
+const shorthand = require("gulp-shorthand");
+const groupCssMediaQueries = require("gulp-group-css-media-queries");
 
 // CSS - обработка
 const css = () => {
-    return src (path.css.src)
+    return src (path.css.src, { sourcemaps: true })
     .pipe(plumber({
         errorHandler: notify.onError(error => ({
             title: "CSS",
@@ -21,7 +27,15 @@ const css = () => {
     }))
     .pipe(concat("main.css"))
     .pipe(cssimport())
-    .pipe(dest(path.css.dest));
+    .pipe(autoprefixer())
+    .pipe(shorthand())
+    .pipe(groupCssMediaQueries())
+    .pipe(size({title: "main.css"}))
+    .pipe(dest(path.css.dest, { sourcemaps: true }))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(csso())
+    .pipe(size({title: "main.min.css"}))
+    .pipe(dest(path.css.dest, { sourcemaps: true }));
 }
 
 module.exports = css; //Экспорт модуля наружу
